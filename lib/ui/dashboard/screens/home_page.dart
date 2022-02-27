cd1a509161e8cc8fe678035a5eea970a99bfa965
devhingu <devhingu786@gmail.com>
2022-02-27 13:19:33 +0530

    bottom sheet, welcome, member list, member detail screens ui

--- lib/ui/dashboard/screens/home_page.dart
+++ lib/ui/dashboard/screens/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gym/constants/color_constants.dart';
+import 'package:gym/ui/dashboard/screens/add_member_screen.dart';
 import 'package:gym/ui/dashboard/screens/home_screen.dart';
 
 class HomePage extends StatefulWidget {
@@ -41,37 +42,32 @@ class _HomePageState extends State<HomePage> {
           color: kWhiteColor,
           size: 30.0,
         ),
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const AddMemberScreen()));
+        },
         elevation: 8.0,
       );
 
   BottomNavigationBar bottomNavBar() => BottomNavigationBar(
-    iconSize: 26.0,
-    showSelectedLabels: false,
-    showUnselectedLabels: false,
-    selectedItemColor: kMainColor,
-    unselectedItemColor: kGreyColor,
-    currentIndex: _selectedIndex,
-    backgroundColor: Colors.white,
-    type: BottomNavigationBarType.fixed,
-    items: const [
-      BottomNavigationBarItem(
-        icon: Icon(CupertinoIcons.home),
-        label: "Home"
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(CupertinoIcons.person_2_fill),
-        label: "Profile"
-      ),
-      BottomNavigationBarItem(
-        icon: Icon(CupertinoIcons.location),
-        label: "Camera"
-      ),
-      BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.profile_circled),
-          label: "Camera"
-      ),
-    ],
-    onTap: _onItemTapped,
-  );
+        iconSize: 26.0,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: kMainColor,
+        unselectedItemColor: kGreyColor,
+        currentIndex: _selectedIndex,
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.person_2_fill), label: "Members"),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.location), label: "Location"),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.profile_circled), label: "Account"),
+        ],
+        onTap: _onItemTapped,
+      );
 }
